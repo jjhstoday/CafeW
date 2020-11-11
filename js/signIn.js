69051eec0e911f9c215854b77399fe887506dadf
HaJunRyu <hjr4557@naver.com>
2020-11-11 13:30:50 +0900

    feat: create signIn.js, because of addClass idSave and autoLogin button background modify signIn.html - bugfix modify signIn.css - bugfix

---
+++ js/signIn.js
@@ -0,0 +1,7 @@
+function idSave() {
+    document.getElementById("id_save").parentElement.parentElement.classList.toggle('idSaveEnable');
+}
+
+function autoLogin() {
+    document.getElementById("auto_login").parentElement.parentElement.classList.toggle('autoLoginEnable');
+}
